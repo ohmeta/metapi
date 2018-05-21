400fa93824cb2f104e5dad7834eb19c9134005e0
alienzj <alienchuj@gmail.com>
2018-05-21 10:39:50 +0800

    update reassembly.py

--- scripts/reassembly.py
+++ scripts/reassembly.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import argparse
+import errno
 import glob
 import os
 import re
@@ -19,80 +20,107 @@ def parse_bins_reads(bins_list, reads_list):
             # reads_id, reads_1, reads_2 = line.strip().split(" ")
             reads_id, reads_1, reads_2 = re.split(r'\s+|\t', line.strip())
             reads_dict[reads_id] = (reads_1, reads_2)
-    return (bins_dict, reads_dict)
+    return bins_dict, reads_dict
 
 
 def index(bins_dict, output_dir):
     index_cmd = {}
     index_dir = os.path.join(output_dir, "index")
-    os.makedirs(index_dir)
+    try:
+        os.makedirs(index_dir)
+    except OSError as e:
+        if e.errno != errno.EEXIST:
+            raise
     for bin_id in bins_dict:
         bin_index_dir = os.path.join(index_dir, bin_id)
-        os.makedirs(bin_index_dir)
+        try:
+            os.makedirs(bin_index_dir)
+        except OSError as e:
+            if e.errno != errno.EEXIST:
+                raise
         prefix = bin_index_dir + "/" + bin_id
-        cmd = "bwa index %s -p %s".format(bins_dict[bin_id], prefix)
+        cmd = "bwa index %s -p %s" % (bins_dict[bin_id], prefix)
         index_cmd[bin_id] = cmd
     return index_cmd
 
 
 def mapping(reads_dict, bins_dict, output_dir):
     mapping_cmd = {}
+    mapped_dict = {}
     mapping_dir = os.path.join(output_dir, "mapping")
-    os.makedirs(mapping_dir)
+    try:
+        os.makedirs(mapping_dir)
+    except OSError as e:
+        if e.errno != errno.EEXIST:
+            raise
     for bin_id in bins_dict:
         mapping_cmd[bin_id] = []
-        prefix = os.path.join(output_dir, "index/%s/%s".format(bin_id, bin_id))
+        mapped_dict[bin_id] = {}
+        mapped_dict[bin_id]["r1"] = []
+        mapped_dict[bin_id]["r2"] = [] 
+        prefix = os.path.join(output_dir, "index/%s/%s" % (bin_id, bin_id))
         bin_mapping_dir = os.path.join(mapping_dir, bin_id)
-        os.makedirs(bin_mapping_dir)
+        try:
+            os.makedirs(bin_mapping_dir)
+        except OSError as e:
+            if e.errno != errno.EEXIST:
+                raise
         for read_id in reads_dict:
-            r1 = os.path.join(bin_mapping_dir, "%s-%s-mapped.1.fq.gz".format(
-                bin_id, read_id))
-            r2 = os.path.join(bin_mapping_dir, "%s-%s-mapped.2.fq.gz".format(
-                bin_id, read_id))
-            cmd = "bwa mem -t %s %s %s | \
-                   tee >(samtools flagstat -@8 - > %s) | \
-                   samtools fastq -@8 -F 12 -n -1 %s -2 %s -".format(
-                prefix, reads_dict[read_id][0], reads_dict[read_id][1], r1, r2)
+            r1 = os.path.join(bin_mapping_dir,
+                              "%s-%s-mapped.r1.fq.gz" % (bin_id, read_id))
+            r2 = os.path.join(bin_mapping_dir,
+                              "%s-%s-mapped.r2.fq.gz" % (bin_id, read_id))
+            stat = os.path.join(bin_mapping_dir,
+                                "%s-%s-flagstat.txt" % (bin_id, read_id))
+            cmd = "bwa mem -t %s %s %s | tee >(samtools flagstat -@8 - > %s) | samtools fastq -@8 -F 12 -n -1 %s -2 %s -" % (
+                prefix, reads_dict[read_id][0], reads_dict[read_id][1], stat,
+                r1, r2)
             mapping_cmd[bin_id].append(cmd)
-    return mapping_cmd
+            mapped_dict[bin_id]["r1"].append(r1)
+            mapped_dict[bin_id]["r2"].append(r2)
+
+    return mapping_cmd, mapped_dict
 
 
-def reassembly(assembly, bins_dict, output_dir):
+def reassembly(assembly, bins_dict, mapped_dict, output_dir):
+    assembly_cmd = {}
     if assembly == "megahit":
-        megahit(bins_dict, output_dir)
+        assembly_cmd = megahit(bins_dict, mapped_dict, output_dir)
     if assembly == "spades":
-        spades(bins_dict, output_dir)
+        assembly_cmd = spades(bins_dict, mapped_dict, output_dir)
     if assembly == "idba_ud":
-        idba_ud(bins_dict, output_dir)
-    else:
-        sys.exit()
+        assembly_cmd = idba_ud(bins_dict, mapped_dict, output_dir)
+    return assembly_cmd
 
 
-def megahit(bins_dict, output_dir):
+def megahit(bins_dict, mapped_dict, output_dir):
     megahit_cmd = {}
     asm_dir = os.path.join(output_dir, "assembly")
-    os.makedirs(asm_dir)
+    try:
+        os.makedirs(asm_dir)
+    except OSError as e:
+        if e.errno != errno.EEXIST:
+            raise
     for bin_id in bins_dict:
-        bin_mapping_dir = os.path.join(output_dir, "mapping/%s".format(bin_id))
         bin_assembly_dir = os.path.join(asm_dir, bin_id)
-        r1_pattern = bin_mapping_dir + "/*.1.fq.gz"
-        r2_pattern = bin_mapping_dir + "/*.2.fq.gz"
-        r1 = ",".join(glob.glob(r1_pattern))
-        r2 = ",".join(glob.glob(r2_pattern))
-        cmd = "megahit -1 %s -2 %s -t 8 --min-contig-len 500 \
-               --out-dir %s --out-prefix %s --continue".format(
+        r1 = ",".join(mapped_dict[bin_id]["r1"])
+        r2 = ",".join(mapped_dict[bin_id]["r2"])
+        cmd = "megahit -1 %s -2 %s -t 8 --min-contig-len 500 --out-dir %s --out-prefix %s --continue" % (
             r1, r2, bin_assembly_dir, bin_id)
         megahit_cmd[bin_id] = cmd
     return megahit_cmd
 
 
-def spades(bins_dict, output_dir):
+def spades(bins_dict, mapped_dict, output_dir):
+    assembly_cmd = {}
     print("comming soon")
+    return assembly_cmd
 
 
-def idba_ud(bins_dict, output_dir):
+def idba_ud(bins_dict, mapped_dict, output_dir):
+    assembly_cmd = {}
     print("comming soon")
-
+    return assembly_cmd
 
 def main():
     parser = argparse.ArgumentParser(description='reassembly reads')
@@ -112,20 +140,25 @@ def main():
     args = parser.parse_args()
 
     bins_dict, reads_dict = parse_bins_reads(args.bl, args.rl)
+    index_tag = False
+    mapping_tag = False
 
     if args.index:
         index_cmd = index(bins_dict, args.o)
+        index_tag = True
 
     if args.mapping:
-        mapping_cmd = mapping(reads_dict, bins_dict, args.o)
+        mapping_cmd, mapped_dict = mapping(reads_dict, bins_dict, args.o)
+        mapping_tag = True
 
-    assembly_cmd = reassembly(args.assembly, bins_dict, args.o)
+    assembly_cmd = reassembly(args.assembly, bins_dict, mapped_dict, args.o)
 
-    if args.index:
-        with open("index.sh", 'w') as index_out, \
-             open("mapping.sh", 'w') as mapping_out, \
-             open("assembly.sh", 'w') as assembly_out, \
-             open("all.sh", 'w') as all_out:
+    if index_tag:
+        with open("index.sh",
+                  'w') as index_out, open(
+                      "mapping.sh", 'w') as mapping_out, open(
+                          "assembly.sh", 'w') as assembly_out, open(
+                              "all.sh", 'w') as all_out:
             for bin_id in bins_dict:
                 index_out.write(index_cmd[bin_id] + "\n")
                 all_out.write(index_cmd[bin_id] + "\n")
@@ -134,12 +167,11 @@ def main():
                     all_out.write(map_cmd + "\n")
                 assembly_out.write(assembly_cmd[bin_id] + "\n")
                 all_out.write(assembly_cmd[bin_id] + "\n")
-
     else:
-        if args.mapping:
-            with open("mapping.sh", 'w') as mapping_out, \
-                 open("assembly.sh", 'w') as assembly_out, \
-                 open("all.sh", 'w') as all_out:
+        if mapping_tag:
+            with open("mapping.sh", 'w') as mapping_out, open(
+                    "assembly.sh", 'w') as assembly_out, open("all.sh",
+                                                              'w') as all_out:
                 for bin_id in bins_dict:
                     for map_cmd in mapping_cmd[bin_id]:
                         mapping_out.write(map_cmd + "\n")
