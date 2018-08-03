aebd4f5b88caf3e0994df7b1a1bb537097de6514
alienzj <alienchuj@gmail.com>
2018-08-03 16:14:22 +0800

    fix bugs

--- scripts/batch_prokka.py
+++ scripts/batch_prokka.py
@@ -8,15 +8,22 @@ import sys
 
 def run(dir_list, outdir, logdir):
     cmd_list = []
-    for dir in dir_list:
-        bin_list = glob.glob(dir + "/*bin*fa")
-        for bin in bin_list:
-            bin_id = os.path.basename(bin).rstrip(".fa")
-            prokka_dir = os.path.join(outdir, bin_id)
-            log = os.path.join(logdir, bin_id + ".prokka.log")
-            cmd = "prokka %s --outdir %s --prefix %s --kingdom Bacteria --cpus 8 2> %s".format(
-                bin.strip(), prokka_dir, bin_id, log)
-            cmd_list.append(cmd)
+    count = 1
+    with open(dir_list) as f:
+        for dir in f:
+            count += 1
+            bin_list = glob.glob(dir.strip() + "/*bin*fa")
+            for bin in bin_list:
+                bin_id = os.path.basename(bin).rstrip(".fa")
+                prokka_dir = os.path.join(outdir,
+                                          os.path.basename(dir.strip()))
+                log = os.path.join(logdir, bin_id + ".prokka.log")
+                cmd = "prokka %s --outdir %s --prefix %s --kingdom Bacteria --cpus 8 2> %s" % (
+                    bin.strip(), prokka_dir, bin_id, log)
+                cmd_list.append(cmd)
+            if count == 2:
+                break
+    return cmd_list
 
 
 cmd_list = run(sys.argv[1], sys.argv[2], sys.argv[3])
