d6690f09a979feb747d9aa544a5920b630843aec
alienzj <alienchuj@gmail.com>
2018-08-05 15:01:25 +0800

    foorbar, pypi issues

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import setuptools
 with open("README.md") as f:
     long_description = f.read()
 
-version = "0.1.2"
+version = "0.1.3"
 print("""------------------------
 Installing metapi version {}
 ------------------------
