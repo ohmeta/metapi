e808ed3b616c8fa8bcbd0b7d25c414bdcbcace1c
alienzj <alienchuj@gmail.com>
2019-03-22 18:11:32 +0800

    add bowtie2 method for rmhost

--- scripts/metaqc.py
+++ scripts/metaqc.py
@@ -17,12 +17,19 @@ TRIM_TEMPLATE = '''fastp --in1 {raw_r1} --in2 {raw_r2} \
 --length_required {length_required} \
 --html {html} --json {json} 2> {trim_log}'''
 
-RMHOST_TEMPLATE = '''bwa mem -t {threads} {host_index_base} \
+RMHOST_BWA_TEMPLATE = '''bwa mem -t {threads} {host_index_base} \
 {trimmed_r1} {trimmed_r2} | \
 tee >(samtools flagstat -@{threads} - > {samflagstat}) | \
 tee >(samtools fastq -@{threads} -N -f 12 -F 256 -1 {rmhosted_r1} -2 {rmhosted_r2} -) | \
 samtools sort -@%{threads} -O BAM -o {sorted_bam} - 2> {rmhost_log}'''
 
+RMHOST_BOWTIE2_TEMPLATE = '''bowtie2 --threads {threads} -x {host_index_base} \
+-1 {trimmed_r1} -2 {trimmed_r2} {additional_params} 2> {rmhost_log} | \
+tee >(samtools flagstat -@{threads} - > {samflagstat}) | \
+tee >(samtools sort -@{threads} -O BAM -o {sorted_bam}) | \
+samtools view -@{threads} -SF4 - | awk -F'[/\t]' '{print a$1}' | sort | uniq | \
+tee >(awk '{print $0 "/1"}' - | seqtk subseq -r {trimmed_r1} - | pigz -p {threads} -c > {rmhosted_r1}) | \
+awk '{print $0 "/2"}' - | seqtk subseq -r {trimmed_r2} - | pigz -p {threads} -c > {rmhosted_r2}'''
 
 def parse_samples(samples_tsv):
     return pd.read_csv(samples_tsv, sep='\s+').set_index("id", drop=False)
@@ -51,8 +58,9 @@ class trimmer:
 
 
 class rmhoster:
-    def __init__(self, sample_id, host_index_base, trim_dir, rmhost_dir):
+    def __init__(self, sample_id, host_index_base, trim_dir, rmhost_dir, params=""):
         self.threads = 8
+        self.additional_params = params
         self.host_index_base = host_index_base
         self.trimmed_r1 = os.path.join(trim_dir, sample_id + ".trimmed.1.fq.gz")
         self.trimmed_r2 = os.path.join(trim_dir, sample_id + ".trimmed.2.fq.gz")
@@ -81,7 +89,14 @@ def main():
         '-d',
         '--database',
         type=str,
+        default=None,
         help='host index base path')
+    parser.add_argument(
+        '-m',
+        '--aligner',
+        type=str,
+        choices=["bwa", "bowtie2"],
+        help='which aligner to do rmhost')
     parser.add_argument(
         '-o',
         '--output',
@@ -103,7 +118,17 @@ def main():
         r2 = get_fqpath(samples_df, sample_id, "fq2")
 
         trim_cmd = TRIM_TEMPLATE.format_map(vars(trimmer(sample_id, r1, r2, trim_outdir)))
-        rmhost_cmd = RMHOST_TEMPLATE.format_map(vars(rmhoster(sample_id, args.database, trim_outdir, rmhost_outdir)))
+        if args.database is not None:
+            if args.aligner == "bwa":
+                rmhost_cmd = RMHOST_BWA_TEMPLATE.format_map(
+                    vars(rmhoster(sample_id, args.database, trim_outdir, rmhost_outdir)))
+            elif args.aligner == "bowtie2":
+                rmhost_cmd = RMHOST_BOWTIE2_TEMPLATE.format_map(
+                    vars(rmhoster(sample_id, args.database, trim_outdir, rmhost_outdir, "--no-unal")))
+            else:
+                rmhost_cmd = ""
+        else:
+            rmhost_cmd = ""
         print(trim_cmd)
         print(rmhost_cmd)
 
