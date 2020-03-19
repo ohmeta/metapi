221bf56dd29768edac2d3ccf8c521e9e8840dc6f
alienzj <alienchuj@gmail.com>
2020-03-19 08:56:16 +0800

    update checkmer.py, save

--- metapi/checkmer.py
+++ metapi/checkmer.py
@@ -2,21 +2,55 @@
 
 import pandas as pd
 import concurrent.futures
+import re
 import os
 import sys
 import argparse
 
-def parse():
-    pass
+
+def parse(checkm_file):
+    total_list = []
+    with open(checkm_file, "r") as ih:
+        next(ih), next(ih), next(ih)
+        for line in ih:
+            if not line.startswith("--"):
+                line_list = re.split(r"\s+", line.strip())
+                total_list.append(
+                    [line_list[0]] + ["-".join(line_list[1:3])] + line_list[3:15]
+                )
+
+    checkm_df = pd.DataFrame(
+        total_list,
+        columns=[
+            "bin_id",
+            "marker_lineage",
+            "genomes",
+            "markers",
+            "marker_sets",
+            "0",
+            "1",
+            "2",
+            "3",
+            "4",
+            "5+",
+            "completeness",
+            "contamination",
+            "strain_heterogeneity",
+        ],
+    )
+    return checkm_df
 
 
-def report(checkm_list, output, completeness, contamination):
-    pass
+def report(checkm_list, output, completeness, contamination, threads):
+    df_list = []
+    with concurrent.futures.ProcessPoolExecutor(max_works=threads) as executor:
+        for df in executor.map(parse, checkm_list):
+            if not df.empty():
+                df_list.append(df)
 
-
-def main():
-    pass
-
-
-if __name__ == '__main__':
-    main()
+    df_ = pd.concat(df_list)
+    df_ = df_.sort_values(
+        by=["completeness", "contamination", "strain_heterogeneity"],
+        ascending=[False, True, True],
+    )
+    return df_
