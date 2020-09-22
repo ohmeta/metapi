8e00c3bc057eaf66b1432d0717224b2ea1a79fbe
alienzj <alienchuj@gmail.com>
2020-09-22 12:28:54 +0800

    use f-str

--- metapi/sampler.py
+++ metapi/sampler.py
@@ -4,6 +4,7 @@ import gzip
 import os
 import sys
 import time
+
 import pandas as pd
 from Bio import bgzf
 
@@ -17,33 +18,29 @@ def parse_samples(config):
     if "fq1" in samples_df.columns:
         for sample_id in samples_df.index.unique():
             if "." in sample_id:
-                print(
-                    "{sample_id} contain '.', please remove '.', now quiting :)".format(
-                        sample_id=sample_id
-                    )
-                )
+                print(f"{sample_id} contain '.', please remove '.', now quiting :)")
                 cancel = True
             fq1_list = samples_df.loc[[sample_id], "fq1"].dropna().tolist()
             fq2_list = samples_df.loc[[sample_id], "fq2"].dropna().tolist()
             for fq_file in fq1_list:
                 if not fq_file.endswith(".gz"):
-                    print("{fq_file} need gzip format".format(fq_file=fq_file))
+                    print(f"{fq_file} need gzip format")
                     cancel = True
                 if not os.path.exists(fq_file):
-                    print("{fq_file} not exists".format(fq_file))
+                    print(f"{fq_file} not exists")
                     cancel = True
                 if (config["params"]["reads_layout"] == "pe") and (
                     not config["params"]["interleaved"]
                 ):
                     if len(fq2_list) == 0:
-                        print("{sample_id} fq2 not exists".format(sample_id=sample_id))
+                        print(f"{sample_id} fq2 not exists")
                         cancel = True
     elif "sra" in samples_df.columns:
         for sample_id in samples_df.index.unique():
             sra_list = samples_df.loc[[sample_id], "sra"].dropna().tolist()
             for sra_file in sra_list:
                 if not os.path.exists(sra_file):
-                    print("{sra_file} not exists".format(sra_file=sra_file))
+                    print(f"{sra_file} not exists")
                     cancel = True
     else:
         print("wrong header: {header}".format(header=samples_df.columns))
