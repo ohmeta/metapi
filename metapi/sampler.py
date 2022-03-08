f5871a27e6e438fe275b2c1cc8cfa3b7c7ee64d7
alienzj <alienchuj@gmail.com>
2022-03-08 14:58:32 +0800

    sepcific sample_id type

--- metapi/sampler.py
+++ metapi/sampler.py
@@ -9,16 +9,21 @@ import pandas as pd
 from Bio import bgzf
 
 
-def parse_samples(samples_tsv,
-                  interleaved="false",
-                  reads_layout="pe",
-                  begin_point="trimming",
-                  check_samples=False):
-    samples_df = pd.read_csv(samples_tsv, sep="\t").set_index("id", drop=False)
+def parse_samples(
+    samples_tsv,
+    interleaved="false",
+    reads_layout="pe",
+    begin_point="trimming",
+    check_samples=False,
+):
+    samples_df = pd.read_csv(
+        samples_tsv, sep="\t", dtype={"id": str, "fq1": str, "fq2": str}
+    ).set_index("id", drop=False)
 
     cancel = False
     if "fq1" in samples_df.columns:
         for sample_id in samples_df.index.unique():
+            sample_id = str(sample_id)
             if "." in sample_id:
                 print(f"{sample_id} contain '.', please remove '.', now quiting :)")
                 cancel = True
@@ -33,14 +38,13 @@ def parse_samples(samples_tsv,
                     if not os.path.exists(fq_file):
                         print(f"{fq_file} not exists")
                         cancel = True
-                    if (reads_layout == "pe") and (
-                        not interleaved
-                    ):
+                    if (reads_layout == "pe") and (not interleaved):
                         if len(fq2_list) == 0:
                             print(f"{sample_id} fq2 not exists")
                             cancel = True
     elif "sra" in samples_df.columns:
         for sample_id in samples_df.index.unique():
+            sample_id = str(sample_id)
             if "." in sample_id:
                 print(f"{sample_id} contain '.', please remove '.', now quiting :)")
                 cancel = True
@@ -58,12 +62,14 @@ def parse_samples(samples_tsv,
     if begin_point == "binning":
         if len(samples_df) != len(samples_df.index.unique()):
             print(
-                "when begin with binning, samples id need to be unique, because we can't merge assembly")
+                "when begin with binning, samples id need to be unique, because we can't merge assembly"
+            )
             cancel = True
 
         if check_samples:
             if "scaftigs" in samples_df.columns:
                 for sample_id in samples_df.index.unique():
+                    sample_id = str(sample_id)
                     scaftigs = samples_df.loc[sample_id, "scaftigs"]
                     if not os.path.exists(scaftigs):
                         print(f"{scaftigs} not exists")
