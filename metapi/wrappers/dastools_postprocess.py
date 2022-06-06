8d56fb4a1f5b66bb6a32332f44ab7c92f62017b6
alienzj <alienchuj@gmail.com>
2022-06-06 13:42:21 +0800

    more robust parsing dastools results

--- metapi/wrappers/dastools_postprocess.py
+++ metapi/wrappers/dastools_postprocess.py
@@ -12,7 +12,8 @@ bins_list = glob.glob(os.path.join(sys.argv[1] + "_DASTool_bins", "*.fa"))
 
 if len(bins_list) > 0:
     for bin_fa in bins_list:
-        binner = os.path.basename(bin_fa).split(".")[0]
-        if binner != "unbinned":
-            bin_fa_ = bins_prefix + os.path.basename(bin_fa).replace(binner, binner +"_dastools")
-            subprocess.run(f'''mv {bin_fa} {bin_fa_}''', shell=True)
+        if (os.path.getsize(bin_fa) > 0) and  (not "*" in bin_fa):
+            binner = os.path.basename(bin_fa).split(".")[0]
+            if (binner != "unbinned") and (binner != "*"):
+                bin_fa_ = bins_prefix + os.path.basename(bin_fa).replace(binner, binner +"_dastools")
+                subprocess.run(f'''mv {bin_fa} {bin_fa_}''', shell=True)
