99cfd6166931b2b50348faefcb214cfc099f5085
alienzj <alienchuj@gmail.com>
2022-11-08 15:49:28 +0800

    prepare raw can work

--- metapi/wrappers/preprocess_raw.py
+++ metapi/wrappers/preprocess_raw.py
@@ -41,7 +41,6 @@ if reads_format == "fastq":
                 sp.run(f'''cat {fq1_str} > {fq1_} 2> {log}''', shell=True)
                 sp.run(f'''cat {fq2_str} > {fq2_} 2>> {log}''', shell=True)
 
-            
             if check_paired:
                 id1 = f'''{output_dir}/id.list.1'''
                 id2 = f'''{output_dir}/id.list.2'''
@@ -66,12 +65,10 @@ if reads_format == "fastq":
 
                     oneline = gzip.open(fq1_, 'rt').readline().strip().split()[0]
                     if "/1" in oneline:
-                        sp.run(
-                            f'''seqkit grep -f <(awk '{{print $0 "/1"}}' {idp}) {fq1_} -o {fq_1} 2>> {log}''',
-                            shell=True)
-                        sp.run(
-                            f'''seqkit grep -f <(awk '{{print $0 "/2"}}' {idp}) {fq2_} -o {fq_2} 2>> {log}''',
-                            shell=True)
+                        cmd1 = f'''awk '{{print $0 "/1"}}' {idp} | seqkit grep -f - {fq1_} -o {fq_1} 2>> {log}'''
+                        cmd2 = f'''awk '{{print $0 "/2"}}' {idp} | seqkit grep -f - {fq2_} -o {fq_2} 2>> {log}'''
+                        sp.run(cmd1, shell=True)
+                        sp.run(cmd2, shell=True)
                     else:
                         sp.run(f'''seqkit grep -f {idp} {fq1_} -o {fq_1} 2>> {log}''', shell=True)
                         sp.run(f'''seqkit grep -f {idp} {fq2_} -o {fq_2} 2>> {log}''', shell=True)
