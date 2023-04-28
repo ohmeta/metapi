7fe676941851949f281818d92eabee1e6c357b04
alienzj <alienchuj@gmail.com>
2023-04-28 14:20:15 +0800

    update sampels parsing

--- metapi/sampler.py
+++ metapi/sampler.py
@@ -1,84 +1,118 @@
 #!/usr/bin/env python
 import glob
-import gzip
 import os
 import sys
-import time
 
 import pandas as pd
-from Bio import bgzf
 
 
-def parse_samples(
-    samples_tsv,
-    interleaved="false",
-    reads_layout="pe",
-    begin_point="trimming",
-    check_samples=False,
-):
-    samples_df = pd.read_csv(
-        samples_tsv, sep="\t", dtype={
-            "sample_id": str,
-            "fq1": str, "fq2": str,
-            "assembly_group": str,
-            "binning_group": str})
+def pass_gzip_format(fq_list):
+    cancel = False
+    for fq in fq_list:
+        if pd.isna(fq):
+            pass
+        elif not fq.endswith(".gz"):
+            cancel = False
+            print(f"{fq} is not gzip format")
+    return cancel
+
+
+def parse_samples(samples_tsv):
+    samples_df = pd.read_csv(samples_tsv, sep="\t")
+    samples_df = samples_df.applymap(str)
+
     samples_df["multibinning_group"] = samples_df["assembly_group"] + "__" + samples_df["binning_group"]
     samples_df = samples_df.set_index(["sample_id", "assembly_group", "binning_group", "multibinning_group"])
 
     cancel = False
-    if "fq1" in samples_df.columns:
-        for sample_id in samples_df.index.get_level_values("sample_id").unique():
-            sample_id = str(sample_id)
-            if "." in sample_id:
-                print(f"{sample_id} contain '.', please remove '.', now quiting :)")
-                cancel = True
 
-            fq1_list = samples_df.loc[sample_id, :, :, :]["fq1"].dropna().tolist()
-            fq2_list = samples_df.loc[sample_id, :, :, :]["fq2"].dropna().tolist()
-            for fq_file in fq1_list:
-                if not fq_file.endswith(".gz"):
-                    print(f"{fq_file} need gzip format")
-                    cancel = True
-                if check_samples:
-                    if not os.path.exists(fq_file):
-                        print(f"{fq_file} not exists")
-                        cancel = True
-                    if (reads_layout == "pe") and (not interleaved):
-                        if len(fq2_list) == 0:
-                            print(f"{sample_id} fq2 not exists")
-                            cancel = True
-    elif "sra" in samples_df.columns:
-        for sample_id in samples_df.index.get_level_values("sample_id").unique():
-            sample_id = str(sample_id)
-            if "." in sample_id:
-                print(f"{sample_id} contain '.', please remove '.', now quiting :)")
-                cancel = True
+    sra_headers = [
+        "sra_pe",
+        "sra_se",
+        "sra_long"
+    ]
 
-            if check_samples:
-                sra_list = samples_df.loc[sample_id, :, :, :]["sra"].dropna().tolist()
-                for sra_file in sra_list:
-                    if not os.path.exists(sra_file):
-                        print(f"{sra_file} not exists")
-                        cancel = True
-    else:
-        print("wrong header: {header}".format(header=samples_df.columns))
+    fastq_headers = [
+        "short_forward_reads",
+        "short_reverse_reads",
+        "short_intervead_reads",
+        "short_single_reads",
+        "long_reads"
+    ]
+
+    # check header
+    is_sra = False
+    is_fastq = False
+    for sra in sra_headers:
+        if sra in samples_df.columns:
+            is_sra = True
+    for fq in fastq_headers:
+        if fq in samples_df.columns:
+            is_fastq = True
+    if is_sra and is_fastq:
         cancel = True
+        print("Can't process sra and fastq at the same time, please provide fastq only or sra only")
+    if cancel:
+        sys.exit(-1)
 
-    if begin_point == "binning":
-        if len(samples_df) != len(samples_df.index.unique()):
-            print(
-                "when begin with binning, samples id need to be unique, because we can't merge assembly"
-            )
+    # check sample_id
+    for sample_id in samples_df.index.get_level_values("sample_id").unique():
+        if "." in sample_id:
+            if "." in sample_id:
+                print(f"{sample_id} contain '.', please remove '.'")
+                cancel = True
+    if cancel:
+        print(f"{samples_tsv} sample_id contain '.' please remove '.', now quiting :)")
+
+    # check fastq headers
+    if is_fastq:
+        if ("short_forward_reads" in samples_df.columns) and ("short_reverse_reads" in samples_df.columns):
+            if "short_intervedad_reads" in samples_df.columns:
+                cancel = True
+                print("can't specific short_forward_reads, short_reverse_reads and short_intervead_reads at the same time")
+                print("please only provide short_forward_reads and short_reverse_reads, or short_intervead_reads")
+            else:
+                pass
+        elif ("short_forward_reads" in samples_df.columns) or ("short_rverse_reads" in samples_df.columns):
             cancel = True
+            print("Please specific short_forward_reads and short_reverse_reads at the same time")
+        else:
+            pass
 
-        if check_samples:
-            if "scaftigs" in samples_df.columns:
-                for sample_id in samples_df.index.unique():
-                    sample_id = str(sample_id)
-                    scaftigs = samples_df.loc[sample_id, "scaftigs"]
-                    if not os.path.exists(scaftigs):
-                        print(f"{scaftigs} not exists")
+    # check reads_format
+    for sample_id in samples_df.index.get_level_values("sample_id").unique():
+        # check short paired-end reads
+        if ("short_forward_reads" in samples_df.columns) and ("short_reverse_reads" in samples_df.columns):
+            forward_reads = samples_df.loc[sample_id, :, :, :]["short_forward_reads"].tolist()
+            reverse_reads = samples_df.loc[sample_id, :, :, :]["short_reverse_reads"].tolist()
+
+            for forward_read, reverse_read in zip(forward_reads, reverse_reads):
+                if pd.isna(forward_read) and pd.isna(reverse_read):
+                    pass
+                elif (not pd.isna(forward_read)) and (not pd.isna(reverse_read)):
+                    if forward_read.endswith(".gz") and reverse_read.endswith(".gz"):
+                        pass
+                    else:
                         cancel = True
+                        print(f"{forward_read} or {reverse_read} is not gzip format")
+                else:
+                    cancel = True
+                    print(f"It seems short paired-end reads only specific forward or reverse reads, please check again!")
+
+        # check short single-end reads
+        if "short_single_reads" in samples_df.columns:
+            single_reads = samples_df.loc[sample_id, :, :, :]["short_single_reads"].tolist()
+            cancel = pass_gzip_format(single_reads)
+
+        # check long reads
+        if "long_reads" in samples_df.columns:
+            long_reads = samples_df.loc[sample_id, :, :, :]["long_reads"].tolist()
+            cancel = pass_gzip_format(long_reads)
+
+        # check short_interleaved
+        if "short_interleaved" in samples_df.columns:
+            interleaved_reads = samples_df.loc[sample_id, :, :, :]["long_reads"].tolist()
+            cancel = pass_gzip_format(interleaved_reads)
 
     if cancel:
         sys.exit(-1)
