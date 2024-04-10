d8f3ecdb9f4260ca8b579d006dddb4df7d86f957
Jie Zhu <alienchuj@gmail.com>
2024-04-10 10:19:58 +0800

    add rule name

--- metapi/corer.py
+++ metapi/corer.py
@@ -45,6 +45,8 @@ WORKFLOWS_MAG = [
     "assembly_report_all",
     "assembly_all",
     "alignment_base_depth_all",
+    "alignment_scaftigs_reads_report_all",
+    "alignment_scaftigs_reads_multi_report_all",
     "alignment_all",
     "binning_metabat2_coverage_all",
     "binning_metabat2_all",
